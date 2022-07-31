0e6349a88970028bf1a3797456f583ad28762730
David Henrique Araujo <95188952+Davidharaujo@users.noreply.github.com>
2022-07-31 20:13:37 -0300

    novas functions

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,36 +4,30 @@ import styled from "styled-components";
 const Container = styled.section `
     background-color: #14213d;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     height: 100vh;
+    width: 100%;
     gap: 20px;
+
+    h2 {
+        color: #fff;
+        text-transform: uppercase;
+        font-size: 40px;
+    }
 `
 const Caixaentrada = styled.div `
     display: flex;
     flex-flow: row wrap;
-    width: 25vw;
-    height: 400px;
+    width: 350px;
+    height: 450px;
     border: solid;
     border-radius: 10px;
     background-color: #000;
     justify-content: center;
     align-items: center;
-
-    input {
-        background-color: #e5e5e5;
-        font-size: 38px;
-        height: 40px;
-        width: 20vw;
-        border: none;
-        border-radius: 2px;
-    }
-
-    input::-webkit-outer-spin-button,
-    input::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-    }
+    
 
     button {
         border-radius: 100%;
@@ -47,76 +41,135 @@ const Caixaentrada = styled.div `
         cursor: pointer;
     }
 `
-const Caixaresultado = styled.p `
-    background-color: #fefae0;
-    border-radius: 10px;
-    font-size: 40px;
+
+const Visor = styled.div `
+        display: flex;
+        background-color: #e5e5e5;
+        font-size: 38px;
+        height: 50px;
+        width: 300px;
+        border: none;
+        border-radius: 5px;
+        align-items: center;
+
+        p {
+            font-family: 'Calculator', sans-serif;
+            color: green;
+            font-size: 40px;
+        }
+     
 `
 
 export default class Main extends Component {
     state = {
         valor1: "",
         valor2: "",
-        result:0
+        result:'',
+        sinal: "",
+        sinalIgual: "",
+        trocaValor: 0,
+        alerta:""
     }
 
-    changeUm = (event) => {
-        this.setState({
-            valor1: Number(event.target.value)
-        })
+    Calc = (event) => {
+        if (this.state.valor1 == "") {
+          this.setState({
+            alerta:"Digite um valor"
+          })
+        } else if (this.state.trocaValor === 0) {
+          this.setState({
+            sinal: event.target.innerHTML,
+            trocaValor: this.state.trocaValor + 1
+          })
+        } else if (this.state.trocaValor === 1) {
+            this.setState ({
+                valor1: this.state.result,
+                sinal: event.target.innerHTML,
+                valor2:'',
+                result:''
+            })
+        }
     }
 
-    changeDois = (event) => {
-        this.setState({
-            valor2: Number(event.target.value)
-        })
+    Equal = (event) => {
+        if (this.state.sinal == "+") {
+            this.setState ({
+                result: Number(this.state.valor1) + Number(this.state.valor2),
+                sinalIgual:'='
+            })
+        }else if (this.state.sinal == "-") {
+            this.setState ({
+                result: Number(this.state.valor1) - Number(this.state.valor2),
+                sinalIgual:'='
+            })
+        }else if (this.state.sinal == "x") {
+            this.setState ({
+                result: Number(this.state.valor1) * Number(this.state.valor2),
+                sinalIgual:'='
+            })
+        }else if (this.state.sinal == "÷") {
+            this.setState ({
+                result: Number(this.state.valor1) / Number(this.state.valor2),
+                sinalIgual:'='
+            })
+        }
     }
 
-    soma = () => {
-        this.setState({
-            result: this.state.valor1 + this.state.valor2,
-        })
+    Number = (event) => {
+        if (this.state.trocaValor === 0) {
+          this.setState({
+            valor1: this.state.valor1 + event.target.innerHTML,
+            alerta:"",
+          })
+        } else if (this.state.trocaValor === 1) {
+          this.setState({
+            valor2: this.state.valor2 + event.target.innerHTML
+          })
+        }
     }
 
-    subtrai = () => {
-        this.setState({
-            result: this.state.valor1 - this.state.valor2,
-        })
-    }
-
-    multiplica = () => {
-        this.setState({
-            result: this.state.valor1 * this.state.valor2,
-        })
-    }
-
-    divide = () => {
-        this.setState({
-            result: this.state.valor1 / this.state.valor2,
-        })
-    }
-
-    clear = () => {
+    Clear = () => {
         this.setState ({
             valor1: "",
             valor2: "",
-            result: 0
+            result:'',
+            sinal: "",
+            sinalIgual: "",
+            trocaValor: 0,
+            alerta:""
         })
     }
 
-    render () {
+    render() {
         return (
             <Container>
+                <h2>Calculator</h2>
                 <Caixaentrada>
-                    <input type='number' value={this.state.valor1} onChange={this.changeUm}></input>
-                    <input type='number' value={this.state.valor2} onChange = {this.changeDois}></input>
-                    <button onClick={this.soma}>+</button>
-                    <button onClick={this.subtrai}>-</button>
-                    <button onClick={this.multiplica}>*</button>
-                    <button onClick={this.divide}>/</button>
-                    <button onClick={this.clear}>C</button>  
+                    <Visor>
+                        <p>{this.state.alerta}</p>
+                        <p>{this.state.valor1}</p>
+                        <p>{this.state.sinal}</p>
+                        <p>{this.state.valor2}</p>
+                        <p>{this.state.sinalIgual}</p>
+                        <p>{this.state.result}</p>
+                    </Visor>
+                    <button onClick={this.Number}>7</button>
+                    <button onClick={this.Number}>8</button>
+                    <button onClick={this.Number}>9</button>
+                    <button onClick={this.Calc}>+</button>
+                    <button onClick={this.Number}>4</button>
+                    <button onClick={this.Number}>5</button>
+                    <button onClick={this.Number}>6</button>
+                    <button onClick={this.Calc}>-</button>
+                    <button onClick={this.Number}>1</button>
+                    <button onClick={this.Number}>2</button>
+                    <button onClick={this.Number}>3</button>
+                    <button onClick={this.Calc}>x</button>
+                    <button onClick={this.Number}>0</button>
+                    <button onClick={this.Equal}>=</button>
+                    <button onClick={this.Clear}>C</button>
+                    <button onClick={this.Calc}>÷</button>
                 </Caixaentrada>
-                <Caixaresultado> = {this.state.result}</Caixaresultado>
             </Container>
         )
     }
